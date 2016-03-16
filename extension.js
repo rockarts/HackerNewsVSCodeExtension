3dcbeac281656d65e03a4683312ea1804ac68f55
Steven Rockarts <srockarts@invidi.com>
2016-03-16 10:12:15 -0600

    Working HTML Preview Spike

--- extension.js
+++ extension.js
@@ -2,35 +2,33 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var Firebase = require('firebase');
-var json2html = require('node-json2html');
 
-this.statusBarItemMain = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 6);
-this.statusBarItemMain.text = '$(home) Hacker News'
-this.statusBarItemMain.tooltip = 'HackerNews Top Stories';
-this.statusBarItemMain.command = 'hackernews.topstories';
-this.statusBarItemMain.show();
+//this.statusBarItemMain = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 6);
+//this.statusBarItemMain.text = '$(home) Hacker News'
+//this.statusBarItemMain.tooltip = 'HackerNews Top Stories';
+//this.statusBarItemMain.command = 'hackernews.topstories';
+//this.statusBarItemMain.show();
 
 function loadTopStories() {
 
     var myFirebaseRef = new Firebase("https://hacker-news.firebaseio.com/v0");
 
-        myFirebaseRef.child("topstories").on("value", function(snapshot) {
-            snapshot.val().forEach(function(element) {
-                var itemValue = "item/" + element;
-                myFirebaseRef.child(itemValue).on("value", function(snapshot) {
-                    //console.log(snapshot.val());  
-                });
-            }, this);
-        });
-        
-	var data = [{'male':'Bob','female':'Jane'},{'male':'Rick','female':'Ann'}];
- 
-	var transform = {'tag':'div','html':'${male} likes ${female}'};
-        
-	var html = json2html.transform(data,transform);
-    var uri = vscode.Uri.parse('css-preview://<html><body><h1>This is a test</h1></body></html>');
-    var success = vscode.commands.executeCommand('vscode.previewHtml', uri);
-    console.log(success)
+    myFirebaseRef.child("topstories").on("value", function(snapshot) {
+        snapshot.val().forEach(function(element) {
+            var itemValue = "item/" + element;
+            myFirebaseRef.child(itemValue).on("value", function(snapshot) {
+                console.log(snapshot.val());  
+            });
+        }, this);
+    });
+
+    const docProvider = {
+		provideTextDocumentContent: () => "<html><body><h1>This is a test</h1></body></html>"
+	};
+	vscode.workspace.registerTextDocumentContentProvider('hackernews', docProvider);
+	vscode.commands.executeCommand("vscode.previewHtml",
+			vscode.Uri.parse("hackernews://base"), vscode.ViewColumn.One)
+			.then(() => 1, error => console.log(error));
 }
 
 // this method is called when your extension is activated
@@ -45,7 +43,6 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	var disposable = vscode.commands.registerCommand('hackernews.topstories', loadTopStories);
-	
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
