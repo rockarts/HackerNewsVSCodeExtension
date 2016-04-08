040e57cca887ba6c85631381998f66648a490174
Steven Rockarts <Steven@proserpina.local>
2016-04-07 23:45:55 -0600

    Grab the top stories and show them in a new window

--- extension.js
+++ extension.js
@@ -4,44 +4,40 @@ var vscode = require('vscode');
 var Firebase = require('firebase');
 var json2html = require('node-json2html');
 var moment = require('moment');
-//this.statusBarItemMain = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 6);
-//this.statusBarItemMain.text = '$(home) Hacker News'
-//this.statusBarItemMain.tooltip = 'HackerNews Top Stories';
-//this.statusBarItemMain.command = 'hackernews.topstories';
-//this.statusBarItemMain.show();
+const api = new Firebase('https://hacker-news.firebaseio.com/v0')
 
-function loadTopStories() {
+var topStories = []; 
 
-    var myFirebaseRef = new Firebase("https://hacker-news.firebaseio.com/v0");
-    
-    myFirebaseRef.child("topstories").once("value", function(snapshot) {
-        snapshot.val().forEach(function(element) {
-            var itemValue = "item/" + element;
-            return myFirebaseRef.child(itemValue).once("value", function(snapshot) {
-                console.log(moment().format());
-                return transformJson(snapshot.val());
-            });
-        }, this);
-    });
+function load() {
+    loadTopStories(print);
 }
 
-function transformJson(data, html) {
-    //LivingSocial Is Laying Off More Than 50 Percent of Its Staff (recode.net)
-    //31 points by danso 37 minutes ago | 12 comments
+function loadTopStories(callback) {
+    api.child('topstories').on('value', snapshot => {
+        topStoryIds = snapshot.val();
+        callback(print_stories); 
+    })
+    
+}
 
+function print(callback) {
+    for(var i = 0; i < topStoryIds.length; i++){
+        api.child('item/' + topStoryIds[i]).once('value', snapshot => {
+            callback(snapshot.val());
+        })
+    }
+}
+
+function print_stories(data) {
     var transform = {"tag":"div","children":[{"tag":"a","href":"${url}","html":"${title}"}]};
-    // const docProvider = {
-    //                 provideTextDocumentContent: () => json2html.transform(data,transform)
-    //             };
-    // console.log(json2html.transform(data,transform) );
-    // vscode.workspace.registerTextDocumentContentProvider('hackernews', docProvider);
-    // vscode.commands.executeCommand("vscode.previewHtml",
-    //         vscode.Uri.parse("hackernews://base"), vscode.ViewColumn.One)
-    //         .then(() => 1, error => console.log(error));
-    html = json2html.transform(data, transform);
-    console.log("--------------------------------------------------");
-    console.log(html);
-    return html;
+     const docProvider = {
+                     provideTextDocumentContent: () => json2html.transform(data,transform)
+                 };
+
+     vscode.workspace.registerTextDocumentContentProvider('hackernews', docProvider);
+     vscode.commands.executeCommand("vscode.previewHtml",
+             vscode.Uri.parse("hackernews://base"), vscode.ViewColumn.One)
+             .then(() => 1, error => console.log(error));
 }
 
 // this method is called when your extension is activated
@@ -55,7 +51,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('hackernews.topstories', loadTopStories);
+	var disposable = vscode.commands.registerCommand('hackernews.topstories', load);
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
